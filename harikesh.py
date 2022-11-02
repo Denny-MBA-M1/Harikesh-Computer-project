9f0ba026bdd6d51e703cb46b75f162fc459fd043
Denny Mathew Cyriac <107631451+Denny-MBA-M1@users.noreply.github.com>
2022-11-02 22:09:26 +0530

    Update harikesh.py

--- harikesh.py
+++ harikesh.py
@@ -15,11 +15,13 @@ except:
 cur=con.cursor()
 
 #creating a database
-cur.execute('create database emp')
-cur.execute('use emp')
+cur.execute('drop database railway')
+cur.execute('create database if not exists railway')
+cur.execute('use railway')
 
-#creating table for train details
-cur.execute("create table if not exists passenger_dtls(pname varchar(30),train_no int primary key,no_of_psng int,cls varchar(5),destination varchar(15),ticket_cost int,pnrno varchar(5))")
+#creating table for train and passenger details
+cur.execute("create table if not exists passenger_dtls(pname varchar(30),train_no int primary key,no_of_psng int,cls varchar(10),destination varchar(15),ticket_cost int,pnrno varchar(10))")
+cur.execute("create table if not exists train_dtls(tname varchar(30),train_no int,station varchar(15),destination varchar(20),ticket_cost int,ecn int,ac int,slp int)")
 
 #Defining menu function 
 def menu():
@@ -27,7 +29,7 @@ def menu():
           print('\nWelcome To Railway some shit')
           print("Press")
           print("1 To Show Train Details")
-          print("2 To Show All Active Passengers")
+          print("2 To Show Passenger Details")
           print("3 To Add New Passenger Details")
           print("4 To Add New Train Details")
           print("5 For Reservation Of Ticket")
@@ -35,46 +37,106 @@ def menu():
           print("7 To Exit")
           print()
           choice=int(input('Enter your preferred choice:'))
-          
+
           if choice==1:
-               hehe
+               Display_TrainDetails()
           elif choice==2:
-               boi
+               Display_Passenger()
           elif choice==3:
                add_passenger()
           elif choice==4:
-               suiii
-          elif choice==5:
-               brrrrr
-          elif choice==6:
-               why u gae
-          elif choice==7:
-               sys.exit()
+               add_traindetails()  
           else:
                print('Invalid choice Try again')
           
 #adding passenger details
 def add_passenger():
+     try:
+          c=con.cursor()
+          pname=input('Enter Passenger Name:')
+          train_no=input("Enter Train No:")
+          no_of_psng=input('Enter No.of Passengers:')
+          cls=input('Enter Ticket Class:')
+          destination=input('Enter Destination:')
+          ticket_cost=input("Enter Fare:")
+          pnrno=input("Enter pnrno:")
+          data=(pname, train_no, no_of_psng, cls, destination, ticket_cost, pnrno)
+
+          #inserting passenger details 
+          sql='insert into passenger_dtls values(%s,%s,%s,%s,%s,%s,%s)'
+
+          #Executing SQL query
+          c.execute(sql,data)
+
+          #commit() methods to make changes
+          con.commit()
+          print("Passenger Added Successfully")
+          menu()
+     except:
+          print("Enter correct values!!")
+
+#To add details of a new train
+def add_traindetails():
+     try:
+          c=con.cursor()
+          tname=input('Enter Train Name:')
+          train_no=input("Enter Train No:")
+          station=input("Enter Station Name:")
+          destination=input('Enter Destination:')
+          ticket_cost=input("Enter Fare:")
+          ecn=input("Enter no.of seats for economy:")
+          ac=input("Enter no.of seats for ac:")
+          slp=input("Enter no.of seats for sleeper:")
+          data=(tname, train_no, station, destination, ticket_cost, ecn, ac, slp)
+
+          sql='insert into train_dtls values(%s,%s,%s,%s,%s,%s,%s,%s)'
+
+          c.execute(sql,data)
+
+          con.commit()
+          print("Train Details Added Successfully")
+          menu()
+     except:
+          print("Enter correct values!!")
+
+#Function to Display details of all the Passengers
+def Display_Passenger():
+     sql='select*from passenger_dtls'
      c=con.cursor()
-     pname=input('Enter Passenger Name:')
-     train_no=imput("Enter Train No:")
-     no_of_psng=input('Enter No.of Passengers:')
-     cls=input('Enter Ticket Class:')
-     destination=input('Enter Destination:')
-     ticket_cost=input("Enter Fare:")
-     pnrno=input("Enter pnrno:")
-     data=(pname, train_no, no_of_psng, cls, destination, ticket_cost, pnrno)
 
-     #inserting passenger details 
-     sql=insert into passenger_dtls(pname,train_no,no_of_psng,cls,destination,ticket_cost,pnrno)values(%s,%s,%s,%s,%s,%s,%s)
+     c.execute(sql)
+     
+     r=c.fetchall()
+     print()  
+     print("Processing please wait....")
+     import time
+     time.sleep(3)
+     print()
+     print("Printing results....")
+     time.sleep(2)
+     print()
+     for i in r:
+          print("pname:", i[0],"train_no:", i[1],"no_of_psng:",i[2],"cls:",i[3],"destination:",i[4],"ticket_cost:",i[5],"pnrno:",i[6])
 
-     #Executing SQL query
-     c.execute(sql,data)
+     menu()
+def Display_TrainDetails():
+    sql='select*from train_dtls'
+    c=con.cursor()
 
-     #commit() methods to make changes
-     con.commit()
+    c.execute(sql)
+     
+    r=c.fetchall()
+    print()
+    print("Processing please wait....")
+    import time
+    time.sleep(3)
+    print()
+    print("Printing results....")
+    time.sleep(2)
+    print()
+    for i in r:
+        print("tname:",i[0],"train_no:",i[1],"station:",i[2],"destination:",i[3],"ticket_cost:",i[4],"ecn:",i[4],"ac:",i[5],"slp:",i[6])
 
-     print("Employee Added Successfully")
+    menu()
 
-
-
+menu()
